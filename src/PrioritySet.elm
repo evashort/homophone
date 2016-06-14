eb3cdb42ea72ef770b7c8efcc572ee0841206acc
Evan Short <evanshort73@gmail.com>
2016-06-13 23:43:25 -0700

    take the set out of priority set

--- src/PrioritySet.elm
+++ src/PrioritySet.elm
@@ -4,49 +4,33 @@ import List
 import PairingHeap exposing (PairingHeap)
 import Set exposing (Set)
 
-type alias PrioritySet comparable =
-  { heap : PairingHeap comparable ()
-  , set : Set comparable
-  }
+type alias PrioritySet comparable = PairingHeap comparable ()
 
 empty : PrioritySet comparable
-empty = { heap = PairingHeap.empty, set = Set.empty }
+empty = PairingHeap.empty
 
 singleton : comparable -> PrioritySet comparable
-singleton k =
-  { heap = PairingHeap.insert (k, ()) PairingHeap.empty
-  , set = Set.singleton k
-  }
+singleton k = PairingHeap.insert (k, ()) PairingHeap.empty
 
 isEmpty : PrioritySet comparable -> Bool
-isEmpty s = Set.isEmpty s.set
+isEmpty = (==) PairingHeap.empty
 
 insert : comparable -> PrioritySet comparable -> PrioritySet comparable
-insert k s =
-  if Set.member k s.set then s
-  else
-    { heap = PairingHeap.insert (k, ()) s.heap
-    , set = Set.insert k s.set
-    }
+insert k s = PairingHeap.insert (k, ()) s
 
 findMin : PrioritySet comparable -> Maybe comparable
-findMin s = Maybe.map fst <| PairingHeap.findMin s.heap
+findMin = Maybe.map fst << PairingHeap.findMin
 
 deleteMin : PrioritySet comparable -> PrioritySet comparable
 deleteMin s =
-  case PairingHeap.findMin s.heap of
+  case findMin s of
     Nothing -> s
-    Just (k, _) ->
-      { heap = PairingHeap.deleteMin s.heap
-      , set = Set.remove k s.set
-      }
+    Just k ->
+      let newS = PairingHeap.deleteMin s in
+        if findMin newS == Just k then deleteMin newS else newS
 
 fromList : List comparable -> PrioritySet comparable
-fromList xs =
-  let set = Set.fromList xs in
-    { heap = PairingHeap.fromList <| List.map parify <| Set.toList set
-    , set = set
-    }
+fromList = PairingHeap.fromList << List.map parify
 
 parify : t -> (t, ())
 parify x = (x, ())
